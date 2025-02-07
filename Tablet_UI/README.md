d48905a50135a48fe33fb05ddd9c9fba5a7153df
Yue Li <yli718@uottawa.ca>
2025-02-07 06:37:04 -0500

    pivoting to use rosbridge_suite

--- Tablet_UI/README.md
+++ Tablet_UI/README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+need to run source ~/ros2_ws/install/setup.bash
+than run ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+
--- Tablet_UI/src/App.js
+++ Tablet_UI/src/App.js
@@ -1,19 +1,91 @@
+// // src/pages/inMotion.js
+// import React, { useEffect, useState } from 'react';
+// import ROSLIB from 'roslib';
+
+// const InMotion = () => {
+//   const [rosMessage, setRosMessage] = useState('');
+//   const [rosConnected, setRosConnected] = useState(false);
+
+//   // Create the ROSLIB ROS connection object
+//   const ros = new ROSLIB.Ros({
+//     url: 'ws://localhost:9090', // URL of your ROS WebSocket server
+//   });
+
+//   useEffect(() => {
+//     // Connect to the ROS WebSocket server
+//     ros.on('connection', () => {
+//       console.log('Connected to ROS WebSocket server');
+//       setRosConnected(true);
+//     });
+
+//     // Handle disconnection
+//     ros.on('close', () => {
+//       console.log('Disconnected from ROS WebSocket server');
+//       setRosConnected(false);
+//     });
+
+//     // Subscribe to a topic (e.g., /rosout for logging messages)
+//     const listener = new ROSLIB.Topic({
+//       ros,
+//       name: '/rosout',
+//       messageType: 'rosgraph_msgs/Log',
+//     });
+
+//     listener.subscribe((message) => {
+//       console.log('Received message on /rosout:', message);
+//       setRosMessage(message.msg); // Update state with the received message
+//     });
+
+//     // Cleanup when component unmounts
+//     return () => {
+//       listener.unsubscribe();
+//       ros.close();
+//     };
+//   }, []);
+
+//   const sendMessage = () => {
+//     const message = new ROSLIB.Message({ data: 'Hello from React!' });
+
+//     // Publish to a topic (e.g., /my_topic)
+//     const publisher = new ROSLIB.Topic({
+//       ros,
+//       name: '/my_topic',
+//       messageType: 'std_msgs/String',
+//     });
+
+//     publisher.publish(message);
+//   };
+
+//   return (
+//     <div>
+//       <h1>In Motion - ROS 2 Communication</h1>
+//       <p>{rosConnected ? 'Connected to ROS WebSocket Server' : 'Connecting to ROS...'}</p>
+//       <p>Received from ROS 2: {rosMessage}</p>
+//       <button onClick={sendMessage}>Send Message to ROS 2</button>
+//     </div>
+//   );
+// };
+
+// export default InMotion;
+
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/login';  // Import your Login component
-import InMotion from './pages/inMotion';  // Import your InMotion component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login'; // Import Login component
+import InMotion from './pages/inMotion'; // Import InMotion component
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Route for the Login page */}
-        <Route path="/inMotion" element={<InMotion />} /> {/* Route for the InMotion page */}
+        {/* Set Login as the default route */}
+        <Route path="/" element={<Login />} />
+
+        {/* Route for InMotion page */}
+        <Route path="/inMotion" element={<InMotion />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
